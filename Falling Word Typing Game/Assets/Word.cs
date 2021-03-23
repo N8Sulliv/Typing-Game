09f8b6291213ca90cfc39e73b2f06f9794f6a296
NSParadox <76891137+N8Sulliv@users.noreply.github.com>
2021-03-23 15:24:55 -0500

    First Part

---
+++ Falling Word Typing Game/Assets/Word.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Word 
+{
+    public string word;
+    private int typeIndex;
+
+    public Word(string _word)
+    {
+        word = _word;
+        typeIndex = 0;
+    }
+
+    public char getNextLetter()
+    {
+        return word[typeIndex];
+    }
+
+    public void TypeLetter()
+    {
+        typeIndex++;
+        //remove letter on screen
+    }
+
+    public bool WordTyped()
+    {
+        bool wordTyped = (typeIndex >= word.Length);
+        if (wordTyped)
+        {
+            //remove word on screen
+        }
+        return wordTyped;
+    }
+}
---
+++ Falling Word Typing Game/Assets/WordGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordGenerator : MonoBehaviour
+{
+    private static string[] wordlist = { "berserk", "slim", "rod", "numberless", "hapless", "color", "arch", "playground", "waves", "smile", "stupid", "amusement", "contain", "elbow", "recondite", "teaching", "heat", "lively", "screw", "flap", "toys", "alluring", "cute", "spark", "shirt", "whirl", "fireman", "shrug", "mom", "kick", "purple", "trains", "magical", "repulsive", "travel", "drink", "sugar", "cellar", "clean", "aboriginal", "awesome", "questionable", "development", "jam", "territory", "adjoining", "fancy", "cool", "locket", "cry", };
+    public static string GetRandomWord()
+    {
+        int randomIndex = Random.Range(0, wordlist.Length);
+        string randomWord = wordlist[randomIndex];
+
+        return randomWord;
+    }
+
+
+}
